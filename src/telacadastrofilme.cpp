e0529f83182b3db4d3b5df4e997dea174d4cad3e
joaoUltPc <zn_john@hotmail.com>
2018-07-10 11:52:30 -0300

    concerto pequenos bugs e tirado tutores que não são usados da telaCadastroFilme

--- src/telacadastrofilme.cpp
+++ src/telacadastrofilme.cpp
@@ -62,7 +62,7 @@ telaCadastroFilme::telaCadastroFilme(QWidget *parent) :
     controlWarnig = new ControladoWarningTutor(":/tutor/tutores/tutoresTelaCadastrofilme.xml",this);
     seqInicial.append("tutorInicio");
     seqInicial.append("tutorInicioVideo");
-    seqInicial.append("tutorPCalibrationSession");
+//    seqInicial.append("tutorPCalibrationSession");
     seqInicial.append("tutorVideoOpenConfira");
     seqInicial.append("tutorVideoSlider");
     seqInicial.append("tutorVideoOpen");
@@ -2005,7 +2005,7 @@ void telaCadastroFilme::on_cbNoise_clicked()
 void telaCadastroFilme::on_pbConfigure1_2_clicked()
 {
     bool chOk=false;
-    ui->cbPro->isChecked(){
+    if(ui->cbPro->isChecked()){
         if(chFrameBack && chFrameFinal && chFrameProce){
             chOk = true;
         }else{
