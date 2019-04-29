3a0c15d4bbd48912cab096fa64d3ab09fa82f5bb
joao_marcolan <jamarcolan@gmail.com>
2019-04-29 02:32:41 -0300

    Adding licensing in all documents

--- src/EthoWatcher.pro
+++ src/EthoWatcher.pro
@@ -1,5 +1,21 @@
 #-------------------------------------------------
 #
+#
+# EthoWatcher OS is a software to assist study of animal behavior.
+# Copyright (C) 2018  Universidade Federal de Santa Catarina.
+
+# EthoWatcher OS is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # Project created by QtCreator 2016-04-20T18:37:51
 #
 #-------------------------------------------------
--- src/telacadastrofilme.cpp
+++ src/telacadastrofilme.cpp
@@ -1684,7 +1684,24 @@ void telaCadastroFilme::on_SliderThreshold_sliderMoved(int position)
     ui->leTreshold->setText(QString::number( position));
 }
 
-void telaCadastroFilme::on_cbNoise_clicked()
+void telaCadastroFilme::on_cbNoise_clicked()/*
+EthoWatcher OS is a software to assist study of animal behavior.
+Copyright (C) 2018  Universidade Federal de Santa Catarina.
+
+EthoWatcher OS is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 {
 
 }
